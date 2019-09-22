e16558123dd62640a5823efc0ae2dad2c5655199
Estefani Arroyo <estefaniarroyo@Estefanis-MacBook-Pro.local>
2019-09-22 16:45:16 -0400

    added the number scroller

--- myApp/src/views/Blank2.vue
+++ myApp/src/views/Blank2.vue
@@ -1,25 +1,75 @@
 ﻿
 <template>
+    
     <div class="container">
-        <div class="row justify-content-center py-5">
-          <h1>Join Us In Reaching Our Goal</h1>
+      <div class="row justify-content-center py-5">
+          <h1>Help us reach our goal of 300,000 HIV/AIDS tests in Miami</h1>
         </div>
+       <div class="iCountUp center-div">
+    <ICountUp
+      :delay="delay"
+      :endVal="endVal"
+      :options="options"
+      @ready="onReady"
+    />
+  </div> 
     <b-progress height="2rem" :value="75" variant="info" :striped="striped" :animated="animate" class="mt-2"></b-progress>
   </div>
 </template>
 
 
 <script>
+  // export default {
+  //   data() {
+  //     return {
+  //       striped: true,
+  //       animate: true
+  //     }
+  //   }
+  // }
+</script>
+<script type="text/babel">
+  import ICountUp from 'vue-countup-v2';
   export default {
+    name: 'demo',
+    components: {
+      ICountUp
+    },
     data() {
       return {
         striped: true,
-        animate: true
+        animate: true,
+        delay: 1000,
+        endVal: 120500,
+        options: {
+          useEasing: true,
+          useGrouping: true,
+          separator: ',',
+          decimal: '.',
+          prefix: '',
+          suffix: ''
+        }
+      };
+    },
+    methods: {
+      onReady: function(instance, CountUp) {
+        const that = this;
+        instance.update(that.endVal + 100);
       }
     }
-  }
+  };
 </script>
 
 <style scoped>
+.iCountUp {
+    font-size: 12em;
+    margin: 0;
+    color: #4d63bc;
+  }
+  .center-div
+{
+    text-align:center
+}
+
 </style>
  
